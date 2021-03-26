eab6a20502f2f8f8ce7d1bb0dbf3797b2ae2cab9
Matěj Rzehulka <matej.rzehulka@fykos.cz>
2021-03-26 16:18:59 +0100

    proper style

--- countRR.py
+++ countRR.py
@@ -21,15 +21,9 @@ def get_mu_col(energy_col, mu_df):
 def countRR(df, mu_df, rho, d, mass, molar_mass, t_irr, irr_start_str):
     AVOGADRO = float(6.02214076e+23)
     mu = get_mu_col(df["Energy"], mu_df)
-    print(mu.value_counts(dropna=False))
-    # rho, d, mass, molar_mass, t_irr = float(rho), float(d), float(mass), float(molar_mass), float(t_irr)
-    rho = float(rho)
-    d = float(d)
+    rho, d = float(rho), float(d)
     mu = mu.astype(np.float64)
-    # print((mu*rho*d).max())
-    print(f"mu is {mu}")
-    print(f"rho is {rho}")
-    print(f"d is {d}")
+
     k = (mu * rho * d) / (1 - np.exp(- mu * rho * d))
     lam = np.log(2) / df["Half-life [s]"]
     mass = float(mass)
@@ -37,19 +31,11 @@ def countRR(df, mu_df, rho, d, mass, molar_mass, t_irr, irr_start_str):
     N = mass*AVOGADRO/molar_mass
     t_irr = float(t_irr)
     irr_start = pd.to_datetime(irr_start_str, dayfirst=True)
-    print(f" irr start is {irr_start}")
     irr_end = irr_start + pd.to_timedelta(t_irr, "s")
-    print(f"irr end is {irr_end}")
-    print(f"acq. started on {df['Acquisition Started'][0]}")
+
     delta_t = pd.to_datetime(df["Acquisition Started"][0]) - irr_end
     delta_t = delta_t.total_seconds()
     delta_t = float(delta_t)
-    print("foo pyco")
-    print(f"lam is {lam}, type {type(lam)}")
-    print(f"t_irr is {t_irr}, type {type(t_irr)}")
-    print(f"delta_t is {delta_t}, type {type(delta_t)}")
-    print(f"real time is {df['Real Time'][0]}, type {type(df['Real Time'][0])}")
-
 
     df["RR"] = (
                 ((df['Real Time'][0] / df["Live Time"][0])
@@ -58,6 +44,7 @@ def countRR(df, mu_df, rho, d, mass, molar_mass, t_irr, irr_start_str):
                    N * (1-np.exp(-lam*t_irr)) * np.exp(-lam * delta_t)
                    * (1-np.exp(-lam * df["Real Time"][0]))
                    * df["eps"] * df["Ig"]))
+    print("Reaction rates counted successfully.")
     return df
 
 
