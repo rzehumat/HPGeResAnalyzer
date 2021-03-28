d4dbb1be697e308a8ddbd3060a30554ec65043a1
Matěj Rzehulka <matej.rzehulka@fykos.cz>
2021-03-28 14:58:26 +0200

    clean code bloat

--- countRR.py
+++ countRR.py
@@ -30,66 +30,30 @@ def countRR(orig_df, mu_df, rho, d, mass, molar_mass, t_irr, irr_start_str):
     irr_start = pd.to_datetime(irr_start_str, dayfirst=True)
     irr_start = int(time.mktime(irr_start.timetuple()))
     irr_end = irr_start + t_irr
-    
+
     acq_started = pd.to_datetime(orig_df["Acquisition Started"][0])
     delta_t = int(time.mktime(acq_started.timetuple())) - irr_end
 
-    df = orig_df[(orig_df["Half-life [s]"] > 0.1 * delta_t) | (orig_df["FWHM"] > 0)]
-    
-    np.seterr(all="ignore")
-    # unp.seterr(all="ignore")
+    df = orig_df[
+        (orig_df["Half-life [s]"] > 0.1 * delta_t)
+        | (orig_df["FWHM"] > 0)]
+
     AVOGADRO = float(6.02214076e+23)
     mu = get_mu_col(df["Energy"], mu_df)
 
-    # rho, d = float(rho), float(d)
     mu = mu.astype(np.float64)
 
     k = (mu * rho * d) / (1 - unp.exp(- mu * rho * d))
     lam = np.log(2) / df["Half-life [s]"]
-    # mass = float(mass)
-    # molar_mass = float(molar_mass)
     N = mass*AVOGADRO/molar_mass
-    # t_irr = float(t_irr)
-    # irr_end = irr_start + pd.to_timedelta(t_irr, "s")
 
-    # delta_t = delta_t.total_seconds()
-    # delta_t = float(delta_t)
-
-    print("real time is")
-    print(df['Real Time'][0])
-    print("live time is")
-    print(df["Live Time"][0])
-
-    aux1 = 1 / N
-    print(f"aux1 is {aux1}")
-    aux2 = 1 / (1-unp.exp(-lam*t_irr))
-    print(f"aux2 is {aux2}")
-    print(f"lambda is {lam}")
-    print(f"dt is {delta_t}")
-    # lam = lam.to_numpy(dtype=np.float128)
-    gg = lam*delta_t
-    # print(f"lam*delta_t is {gg}")
-    # print(f"gg max is {gg.max()}")
-    # print(f"gg is {gg.value_counts()}")
-    # hh = gg.to_numpy()
-    aux3 = unp.exp(gg) / N
-    # aux3 = calc_uexp(lam, delta_t, N)
-    print(f"aux3 is {aux3}")
-    aux = 1 / (N * (1-unp.exp(-lam*t_irr)) * unp.exp(-lam * delta_t))
-    print(aux)
     df["RR"] = ((df['Real Time'][0] / df["Live Time"][0])
                 * k * lam * df["Area"].to_numpy()
                 / (
                     N * (1-unp.exp(-lam*t_irr)) * unp.exp(-lam * delta_t)
                     * (1-np.exp(-lam * df["Real Time"][0]))
                     * df["eps"] * df["Ig"]))
-#     df["RR"] = (
-#                 ((df['Real Time'][0] / df["Live Time"][0])
-#                  * k * lam * df["Area"])
-#                 / (
-#                    N * (1-unp.exp(-lam*t_irr)) * unp.exp(-lam * delta_t)
-#                    * (1-np.exp(-lam * df["Real Time"][0]))
-#                    * df["eps"] * df["Ig"]))
+
     print("Reaction rates counted successfully.")
     return df
 
@@ -102,9 +66,8 @@ def countRR(orig_df, mu_df, rho, d, mass, molar_mass, t_irr, irr_start_str):
     #             res[i] = unp.exp(lam_arr[i] * delta_t) / N
     #         except OverflowError:
     #             print(f"Overflow at i = {i} which is lam * delta = {lam_arr[i]*delta_t}")
-    #             res[i] = 
+    #             res[i] =
     #     return res
-
 
 
 if __name__ == "__main__":
