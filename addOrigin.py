98cfbbac02a5ca15040381494e671ee481a075d7
Matěj Rzehulka <matej.rzehulka@fykos.cz>
2021-03-28 20:33:47 +0200

    fix fission rr

--- addOrigin.py
+++ addOrigin.py
@@ -1,8 +1,9 @@
-import pandas as pd
-
-
 def addOrigin(df, info, yield_df):
-    cols_drop = ["Update", "Sn(keV)", "sigm_Sn(keV)", "E(level)", "sigm_E(level)", "Literature cut-off date", "Author(s)", "Sp(keV)", "sigm_Sp(keV)", "References since cut-off", "Jp", "ENSDF citation"]
+    cols_drop = [
+        "Update", "Sn(keV)", "sigm_Sn(keV)", "E(level)",
+        "sigm_E(level)", "Literature cut-off date", "Author(s)",
+        "Sp(keV)", "sigm_Sp(keV)", "References since cut-off",
+        "Jp", "ENSDF citation"]
     cols = [item for item in info.columns.to_list() if item not in cols_drop]
 
     df[cols] = info[cols]
